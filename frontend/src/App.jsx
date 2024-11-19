ca08e2ce62583eddd259d48b83a042158ab47765
Bethwel Kiplagat <sjemnyango9@gmail.com>
2024-11-19 14:11:09 +0300

    Frontrnd setup and nav

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
-import React, { useState, useEffect } from 'react';
+// App.js
+// importing tailwind for global availability
+import './styles/globals.css';  // Import Tailwind's CSS
 
+import React from 'react';
+import Navbar from './components/Navbar';  // Adjust the path if needed
+import Footer from './components/Footer';  // Ensure the correct path
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Navbar />  {/* This will render the Navbar */}
+      <h1>Welcome to My Website</h1>
+      {/* Other content of your app */}
+
+      {/* Render the Footer component */}
+      <Footer />
     </div>
   );
 }
