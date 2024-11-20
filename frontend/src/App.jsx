5f4feb0b7059605a670ad9945d3aeb3f6e0faf20
Bethwel Kiplagat <sjemnyango9@gmail.com>
2024-11-20 15:35:36 +0300

    Pages/sections view nice

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,21 @@
-
 // importing tailwind for global availability
-import './styles/globals.css';  // Import Tailwind's CSS
+import './styles/globals.css';  
 import '@fontsource/inter/400.css';
 import '@fontsource/inter/700.css';
-
 import React from 'react';
-import Navbar from './components/Navbar';  // Adjust the path if needed
-import Footer from './components/Footer';  // Ensure the correct path
-import Home from './pages/Home';  // Ensure the correct path
-import About from './pages/About';  // Ensure the correct path
-import Contact from './pages/Contact';  // Ensure the correct path
-import Explore from './pages/Explore';  // Ensure the correct path
+import Navbar from './components/Navbar';  
+import Footer from './components/Footer';  
+import Home from './pages/Home';  
+import About from './pages/About';  
+import Contact from './pages/Contact';  
+import Explore from './pages/Explore';  
 import Partners from './pages/Partners';
 
 function App() {
   return (
     <div>
-      <Navbar />  {/* This will render the Navbar */}
-      {/* <h1>Welcome to My Website</h1> */}
-      {/* Other content of your app */}
-
-
-      {/* Render the Home, About, Contact, and Explore pages */}
+      <Navbar />  
+      {/* Rendering the Home, About, Contact, and Explore pages */}
       <Home />
       <About />
       <Partners />
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -20,9 +20,9 @@ module.exports = {
       animation: {
         fadeIn: 'fadeIn 0.5s ease-out'
       },
-      animation: {
-        scroll: 'scroll 25s linear infinite',
-      },
+      // animation: {
+      //   scroll: 'scroll 25s linear infinite',
+      // },
       // fonts for partners section
       fontFamily: {
         inter: ['Inter', 'sans-serif'],
@@ -36,10 +36,10 @@ module.exports = {
           "0%": { opacity: 0, transform: "translateY(20px)" },
           "100%": { opacity: 1, transform: "translateY(0)" },
         },
-        scroll: {
-          '0%': { transform: 'translateX(100%)' },
-          '100%': { transform: 'translateX(-100%)' },
-        },
+        // scroll: {
+        //   '0%': { transform: 'translateX(100%)' },
+        //   '100%': { transform: 'translateX(-100%)' },
+        // },
         fadeIn: {
           '0%': { opacity: 0 },
           '100%': { opacity: 1 },
