b8e494a4bbf42baa1958f840660c270e75b99bfe
Bethwel Kiplagat <sjemnyango9@gmail.com>
2024-11-25 15:48:53 +0300

    Db revamped

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,11 @@
+
+# libraries and dependanceies / importation
+
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_cors import CORS
-from models import db  # Import 'db' from models
+from models import db 
 from flask_migrate import Migrate
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 
@@ -17,7 +20,8 @@ CORS(app, supports_credentials=True)
 CORS(app)
 
 # Configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'  # Replace with your DB URI
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost:bethwel3001@localhost/my_project_db'
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'your_secret_key'  # Replace with your secure key
 
